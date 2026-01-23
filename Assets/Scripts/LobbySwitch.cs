c3e5df1d19dbdfea267f7d5e1b3b4d2ed0d90c39
BPKV444\Teilnehmer <paula.staab@web.de>
2026-01-23 09:11:40 +0100

    ADD: startscreen ADD: Scripts ADD: Buttons "Exit" und "Ready"

---
+++ Assets/Scripts/LobbySwitch.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// Hänge das Skript an dein Canvas oder ein leeres GameObject in der MainMenu- oder GameOver-Scene
+public class LobbySwitch : MonoBehaviour
+{
+    [Header("Buttons (Drag hier rein)")]
+    public Button exitButton;
+
+    void Start()
+    {
+        // Listener für die Buttons setzen
+        if (exitButton != null)
+            exitButton.onClick.AddListener(NewGame);
+    }
+
+    // "New Game" / Restart: Einfach die Game-Scene laden
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Lobby");  // Ersetze durch den genauen Namen deiner Startszene!
+        // Alternativ mit Index: SceneManager.LoadScene(1);
+    }
+}
